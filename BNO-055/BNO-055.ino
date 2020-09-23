1cb11e6d79802a4e6b3501c68d83e288a231daad
ograsdijk <o.grasdijk@gmail.com>
2020-09-23 09:39:39 -0400

    Create BNO-055.ino

---
+++ BNO-055/BNO-055.ino
@@ -0,0 +1,54 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+void setup(void) 
+{
+  Serial.begin(9600);
+  Serial.println("Orientation Sensor Test"); Serial.println("");
+  
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  
+  delay(1000);
+    
+  bno.setExtCrystalUse(true);
+}
+ 
+void loop(void) 
+{
+//  /* Get a new sensor event */ 
+//  sensors_event_t event; 
+//  bno.getEvent(&event);
+//  
+//  /* Display the floating point data */
+//  Serial.print("X: ");
+//  Serial.print(event.orientation.x, 4);
+//  Serial.print("\tY: ");
+//  Serial.print(event.orientation.y, 4);
+//  Serial.print("\tZ: ");
+//  Serial.print(event.orientation.z, 4);
+//  Serial.println("");
+
+  imu::Quaternion quat = bno.getQuat();
+  /* Display the quat data */
+  Serial.print("qW: ");
+  Serial.print(quat.w(), 4);
+  Serial.print(" qX: ");
+  Serial.print(quat.y(), 4);
+  Serial.print(" qY: ");
+  Serial.print(quat.x(), 4);
+  Serial.print(" qZ: ");
+  Serial.print(quat.z(), 4);
+  Serial.println("");  
+ 
+  delay(100);
+}
